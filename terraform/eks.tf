2f9663368f9948e9567e9eed51783cb1b81baa76
Argjend <bytyqi.argjend@outlook.com>
2026-02-25 20:04:31 +0100

    remove iam from eks file

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -22,18 +22,3 @@ module "eks" {
     }
   }
 }
-
-module "karpenter" {
-  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version = "21.15.1"
-
-  cluster_name = module.eks.cluster_name
-
-  # Determines whether to create pod identity association for Karpenter, which allows it to assume the necessary IAM roles for provisioning nodes.
-  create_pod_identity_association = true
-  create_node_iam_role = true
-  
-  node_iam_role_additional_policies = {
-    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
-}
